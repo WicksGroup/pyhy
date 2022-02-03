ebad696de6a49b1e714fc311f495a3653ab6b323
THOMASANDERSON\cjkri <cjkrill99@gmail.com>
2022-02-03 13:53:52 -0500

    updated default pparray variables

--- tools/inf_GUI_helper.py
+++ tools/inf_GUI_helper.py
@@ -327,7 +327,7 @@ class InfWriter:
                                  f'sourcem {sim_props["sourceMultiplier"]}']
             self.inf['LASER'] += sim_props['tvLaser']  # extends the list, not a numerical addition
 
-        self.inf['PARM'] = ['pparray r u acc rho te ti tr pres zbar sd1 eelc eion',
+        self.inf['PARM'] = ['pparray r pres u rho te ti tr sd1',
                             'parm nstop 5000000',
                             'parm IRDTRN 0',
                             f'parm tstop {sim_props["time_max"] * 1e-9:.2e}',
